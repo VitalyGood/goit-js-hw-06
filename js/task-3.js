67303afd56782750afa67fd37ccf8cfb2dd4ac18
VitalyGood <153685960+VitalyGood@users.noreply.github.com>
2024-04-29 12:46:06 +0300

    Updeta task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,20 @@
 
-class StringBuilder{
+class StringBuilder{ 
+    #value;
     constructor(value) {
-        this.value = value;
+        this.#value = value;
     }
     getValue() {
-        return this.value;
+        return this.#value;
     }
     padEnd(str){
-        this.value += str;
+        this.#value += str;
     }
     padStart(str) {
-        this.value = str + this.value;
+        this.#value = str + this.#value;
     }
     padBoth(str) {
-        this.value = str + this.value + str;
+        this.#value = str + this.#value + str;
     }
 }
 const builder = new StringBuilder(".");
